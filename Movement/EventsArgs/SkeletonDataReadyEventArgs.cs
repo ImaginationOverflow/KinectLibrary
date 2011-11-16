4de257cbf97df9c95fbe1c34aa395b8afc7349e7
Diogo Cardoso <amazing.red@gmail.com>
2011-11-16 17:21:03 +0000

    Added project to solution.

--- Movement/EventsArgs/SkeletonDataReadyEventArgs.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Research.Kinect.Nui;
-
-namespace KinectLibrary.Movement.EventsArgs
-{
-    public class SkeletonDataReadyEventArgs
-    {
-        public SkeletonData SkeletonData { get; set; }
-    }
-}
